79d8362de21a0049af916a1d077fe4513e4693b8
Denis <dengalai20@gmail.com>
2022-05-24 02:18:42 +0300

    Handlers and another updates

---
+++ src/main/java/denis/service/ReplyMessageService.java
@@ -0,0 +1,38 @@
+package denis.service;
+
+import denis.FirstTestBotDjek;
+import denis.model.TextMessage;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboard;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardRemove;
+
+public class ReplyMessageService {
+    private final long chatId;
+    private final FirstTestBotDjek bot;
+
+    public ReplyMessageService(long chatId, FirstTestBotDjek bot) {
+        this.chatId = chatId;
+        this.bot = bot;
+    }
+
+    public void replyMessage(TextMessage textMessage, ReplyKeyboard replyKeyboard) {
+        try {
+            bot.execute(
+                    SendMessage.builder()
+                            .text(textMessage.getValue(textMessage))
+                            .chatId(String.valueOf(chatId))
+                            .replyMarkup(replyKeyboard)
+                            .build());
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void replyMessage(TextMessage textMessage, boolean hideButtons) {
+        replyMessage(textMessage, new ReplyKeyboardRemove(hideButtons));
+    }
+
+    public void replyMessage(TextMessage textMessage) {
+        replyMessage(textMessage,true);
+    }
+}
